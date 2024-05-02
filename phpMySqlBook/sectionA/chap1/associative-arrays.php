cbf99a74e1efb6eb5ab837b9a60918b4170f89bc
oguzyildiz1 <yildizoguz1@gmail.com>
2024-05-02 19:33:51 +0300

    updated

---
+++ phpMySqlBook/sectionA/chap1/associative-arrays.php
@@ -0,0 +1,31 @@
+<?php
+$name = "Ivy";
+$price = 2;
+
+$nutrition = [
+    'fat' => 17,
+    'sugar' => 32,
+    'salt' => 3.3,
+    'protein' => 4,
+];
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Variables</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+i
+
+<body>
+    <h1>The Candy Store</h1>
+    <h2>Nutrition per 100g</h2>
+    <p><?php echo "Fat: {$nutrition['fat']}%"; ?></p>
+    <p><?php echo "Sugar: {$nutrition['sugar']}%"; ?></p>
+    <p><?php echo "Salt: {$nutrition['salt']}%"; ?></p>
+    <p><?php echo "Protein: {$nutrition['protein']}%"; ?></p>
+</body>
+
+</html>
---
+++ phpMySqlBook/sectionA/chap1/updating-arrays-02.php
@@ -0,0 +1,30 @@
+<?php
+$best_sellers = ['Chocolate', 'Mints', 'Fudge', 'Licorice', 'Bubble gum', 'Toffee', 'Jelly beams'];
+
+$best_sellers[2] = 'Cheesecake';
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Variables</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+i
+
+<body>
+    <h1>The Candy Store</h1>
+    <h2>Best Sellers:</h2>
+    <ul>
+        <li><?php echo $best_sellers[0] ?></li>
+        <li><?php echo $best_sellers[1] ?></li>
+        <li><?php echo $best_sellers[2] ?></li>
+        <li><?php echo $best_sellers[3] ?></li>
+        <li><?php echo $best_sellers[4] ?></li>
+        <li><?php echo $best_sellers[5] ?></li>
+        <li><?php echo $best_sellers[6] ?></li>
+    </ul>
+</body>
+
+</html>
