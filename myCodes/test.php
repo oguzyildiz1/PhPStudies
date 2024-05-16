963f7e50a88612cecab7dc6fd18eaf3bf1e4aad0
oguzyildiz1 <yildizoguz1@gmail.com>
2024-05-16 11:13:00 +0300

    last updated

---
+++ myCodes/test.php
@@ -0,0 +1,42 @@
+<?php
+$liste = [["Oğuzhan", "Yıldız", "1983"], ["Orhan", "Yıldız", "1986"], ["Güliz", "Yıldız", "1984"]];
+?>
+
+<html lang="tr-TR">
+
+<head>
+    <meta charset="utf-8">
+    <title>Menu Bars</title>
+    <link rel="stylesheet" href="./test.css">
+</head>
+
+<body>
+
+
+    <div class="main-container">
+        <table>
+            <tr>
+                <th>isim</th>
+                <th>Soy İsim</th>
+                <th>Doğum Yılı</th>
+            </tr>
+            <?php
+            // each liste items is written
+            foreach ($liste as $kisi) {
+                echo "<tr>";
+                foreach ($kisi as $info) {
+                    echo "<td>$info</td>";
+                }
+                echo "</tr>";
+            }
+
+            ?>
+        </table>
+    </div>
+
+
+    <script src="./test.js"></script>
+</body>
+
+
+</html>
